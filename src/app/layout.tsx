f1befb6012e2bb0ccdf7758369059e425712f1a2
Akanshu_dutt <duttakanshu@gmail.com>
2025-08-18 13:44:29 +0530

    Initial commit on akanshu branch

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,26 +1,35 @@
-// src/app/layout.tsx
-import './globals.css'
-import { Inter, Roboto_Mono } from 'next/font/google'
-import type { Metadata } from 'next'
+import type { Metadata } from "next";
+import { Geist, Geist_Mono } from "next/font/google";
+import "./globals.css";
 
-const inter = Inter({ subsets: ['latin'], variable: '--font-inter' })
-const robotoMono = Roboto_Mono({ subsets: ['latin'], variable: '--font-roboto-mono' })
+const geistSans = Geist({
+  variable: "--font-geist-sans",
+  subsets: ["latin"],
+});
+
+const geistMono = Geist_Mono({
+  variable: "--font-geist-mono",
+  subsets: ["latin"],
+});
 
 export const metadata: Metadata = {
-  title: 'DreamSky Tech',
-  description: 'Your App Description',
-}
+  title: "DreamSkyTech",
+  description: "DreamSkyTech offers cutting-edge software solutions and digital innovation to elevate your business in the tech-driven world.",
+};
+ 
 
 export default function RootLayout({
   children,
-}: {
-  children: React.ReactNode
-}) {
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
   return (
     <html lang="en">
-      <body className={`${inter.variable} ${robotoMono.variable} antialiased`}>
-        <main>{children}</main>
+      <body
+        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
+      >
+        {children}
       </body>
     </html>
-  )
+  );
 }
